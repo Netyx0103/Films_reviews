9a2b6247ce93930835ccc058ddc57f0bd476d4b2
_Netyx <81749908+Netyx0103@users.noreply.github.com>
2021-04-23 14:47:37 +0000

    Add files via upload

---
+++ films/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:filmid>/', views.film_reviews, name='film-home'),
+    path('<int:filmid>/essays', views.film_essays, name='film-essay'),
+]
